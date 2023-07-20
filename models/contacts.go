094d19239d9d72ad90f4c51db8f3f6e6b9988ee2
Ерлан <agzam.y@telphin.com>
2023-07-20 15:26:48 +0600

    contact fix

--- models/contacts.go
+++ models/contacts.go
@@ -15,8 +15,8 @@ type ContactList struct {
 type ContactResult struct {
 	ID                string    `json:"ID"`
 	Name              string    `json:"NAME"`
-	SecondName        any       `json:"SECOND_NAME"`
-	LastName          any       `json:"LAST_NAME"`
+	SecondName        string    `json:"SECOND_NAME"`
+	LastName          string    `json:"LAST_NAME"`
 	Phone             []PHONE   `json:"PHONE"`
 	Email             []EMAIL   `json:"EMAIL"`
 	WEB               []WEB     `json:"WEB"`
@@ -28,8 +28,8 @@ type ContactResult struct {
 	LeadID            any       `json:"LEAD_ID"`
 	TypeID            string    `json:"TYPE_ID"`
 	SourceID          string    `json:"SOURCE_ID"`
-	SourceDescription any       `json:"SOURCE_DESCRIPTION"`
-	CompanyID         any       `json:"COMPANY_ID"`
+	SourceDescription string    `json:"SOURCE_DESCRIPTION"`
+	CompanyID         string    `json:"COMPANY_ID"`
 	Birthdate         string    `json:"BIRTHDATE"`
 	Export            string    `json:"EXPORT"`
 	HasPhone          string    `json:"HAS_PHONE"`
@@ -45,13 +45,13 @@ type ContactResult struct {
 	OriginID          any       `json:"ORIGIN_ID"`
 	OriginVersion     any       `json:"ORIGIN_VERSION"`
 	FaceID            any       `json:"FACE_ID"`
-	Address           any       `json:"ADDRESS"`
-	Address2          any       `json:"ADDRESS_2"`
-	AddressCity       any       `json:"ADDRESS_CITY"`
+	Address           string    `json:"ADDRESS"`
+	Address2          string    `json:"ADDRESS_2"`
+	AddressCity       string    `json:"ADDRESS_CITY"`
 	AddressPostalCode any       `json:"ADDRESS_POSTAL_CODE"`
-	AddressRegion     any       `json:"ADDRESS_REGION"`
-	AddressProvince   any       `json:"ADDRESS_PROVINCE"`
-	AddressCountry    any       `json:"ADDRESS_COUNTRY"`
+	AddressRegion     string    `json:"ADDRESS_REGION"`
+	AddressProvince   string    `json:"ADDRESS_PROVINCE"`
+	AddressCountry    string    `json:"ADDRESS_COUNTRY"`
 	AddressLocAddrID  any       `json:"ADDRESS_LOC_ADDR_ID"`
 	UtmSource         any       `json:"UTM_SOURCE"`
 	UtmMedium         any       `json:"UTM_MEDIUM"`
@@ -60,4 +60,5 @@ type ContactResult struct {
 	UtmTerm           any       `json:"UTM_TERM"`
 	LastActivityBy    string    `json:"LAST_ACTIVITY_BY"`
 	LastActivityTime  time.Time `json:"LAST_ACTIVITY_TIME"`
+	UFCRMWHATCRM      any       `json:"UF_CRM_WHATCRM"`
 }
