895aafe819394fc4f190acb9f4de2954b948589d
Ерлан <agzam.y@telphin.com>
2023-08-17 11:59:21 +0600

    + options {height}

--- models/userfield.go
+++ models/userfield.go
@@ -1,14 +1,14 @@
 package models
 
 type UserField struct {
-	Title         string           `json:"TITLE,omitempty"`
-	FieldName     string           `json:"FIELD_NAME,omitempty"`
-	EditFormLabel string           `json:"EDIT_FORM_LABEL,omitempty"`
-	UserTypeID    string           `json:"USER_TYPE_ID,omitempty"`
-	UserFieldType string           `json:"USERFIELD_TYPE,omitempty"`
-	Description   string           `json:"DESCRIPTION,omitempty"`
-	Handler       string           `json:"HANDLER,omitempty"`
-	Options       UserFieldOptions `json:"OPTIONS,omitempty"`
+	Title         string            `json:"TITLE,omitempty"`
+	FieldName     string            `json:"FIELD_NAME,omitempty"`
+	EditFormLabel string            `json:"EDIT_FORM_LABEL,omitempty"`
+	UserTypeID    string            `json:"USER_TYPE_ID,omitempty"`
+	UserFieldType string            `json:"USERFIELD_TYPE,omitempty"`
+	Description   string            `json:"DESCRIPTION,omitempty"`
+	Handler       string            `json:"HANDLER,omitempty"`
+	Options       *UserFieldOptions `json:"OPTIONS,omitempty"`
 }
 
 type UserFieldOptions struct {
