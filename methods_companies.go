42f2a7a36ec27cfa024ca1d8b78b25bbe7e2f8ae
lanyere <agzam.erlan@gmail.com>
2023-12-11 15:09:20 +0600

    create companies

--- methods_companies.go
+++ methods_companies.go
@@ -37,7 +37,7 @@ func (c *Get) Companies(id string) (out []models.CompanyResult, err error) {
 	return
 }
 
-func (c *Update) Companies(in models.CompanyResult) (out MainResult, err error) {
+func (c *Update) Companies(in *models.CompanyResult) (out MainResult, err error) {
 	c.b24.log("UpdateCompanies request is started...")
 	options := callMethodOptions{
 		Method:  fiber.MethodPost,
@@ -51,65 +51,21 @@ func (c *Update) Companies(in models.CompanyResult) (out MainResult, err error) 
 	return
 }
 
-//
-//func (c *Create) Company(in *[]models.Company) (out []models.Company, err error) {
-//	c.b24.log("CustomersMode request is started...")
-//
-//	options := callMethodOptions{
-//		Method:  fiber.MethodPost,
-//		BaseURL: CrmCompanyCreate,
-//		In:      in,
-//		Out:     &models.Company{},
-//		Params:  nil,
-//	}
-//
-//	if len(*in) <= 1 {
-//		options.Out = &models.Company{}
-//		if err = c.b24.makeRequest(options); err != nil {
-//			return
-//		}
-//		out = []models.Company{*options.Out.(*models.Company)}
-//	} else {
-//		options.Out = &models.RequestResponse{}
-//		if err = c.b24.makeRequest(options); err != nil {
-//			return
-//		}
-//		out = options.Out.(*models.RequestResponse).Embedded.Companies
-//	}
-//
-//	c.b24.log("returning the struct...")
-//	return
-//}
-//
-//func (c *Update) Company(companyID string, in *[]models.Company) (out []models.Company, err error) {
-//	c.b24.log("CustomersMode request is started...")
-//
-//	options := callMethodOptions{
-//		Method:  fiber.MethodPatch,
-//		BaseURL: companiesURL,
-//		In:      in,
-//		Out:     &models.Company{},
-//		Params:  nil,
-//	}
-//
-//	if companyID != "" {
-//		options.BaseURL += "/" + companyID
-//		options.In = (*in)[0]
-//
-//		if err = c.b24.makeRequest(options); err != nil {
-//			return
-//		}
-//		out = []models.Company{*options.Out.(*models.Company)}
-//	}
-//
-//	if companyID == "" {
-//		options.Out = &models.RequestResponse{}
-//		if err = c.b24.makeRequest(options); err != nil {
-//			return
-//		}
-//		out = options.Out.(*models.RequestResponse).Embedded.Companies
-//	}
-//
-//	c.b24.log("returning the struct...")
-//	return
-//}
+func (c *Create) Companies(in []models.CompanyResult) (out []models.Company, err error) {
+	c.b24.log("CustomersMode request is started...")
+
+	options := callMethodOptions{
+		Method:  fiber.MethodPost,
+		BaseURL: CrmCompanyAdd,
+		In:      in,
+		Out:     &models.Company{},
+		Params:  nil,
+	}
+
+	if err = c.b24.callMethod(options); err != nil {
+		return
+	}
+	out = []models.Company{*options.Out.(*models.Company)}
+	c.b24.log("returning the struct...")
+	return
+}
